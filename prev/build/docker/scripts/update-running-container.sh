163e6a499dfb779f885b4e36b29e495b2e5e3f33
David Birks <davidebirks@gmail.com>
2017-07-28 13:08:03 -0400

    add archive button

--- prev/build/docker/scripts/update-running-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Assuming the container is named farese-container, this can be used to quickly upgrade
-# the running docker container.
-
-sudo docker pull prbc/farese
-sudo docker stop farese-container
-sudo docker rm farese-container
-sudo docker run -d --name=farese-container --restart=always -p 80:80 prbc/farese
