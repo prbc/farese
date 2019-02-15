4299715cf4786ad57c0267fb11a792834353266e
David Birks <davidebirks@gmail.com>
2019-02-15 00:11:13 -0500

    Improvements for the service worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,25 @@
 module.exports = {
-  "globDirectory": ".",
-  "globPatterns": [
+  skipWaiting: true,
+  clientsClaim: true,
+  globDirectory: ".",
+  globPatterns: [
     "**/*.{html,css,png,htm,gif,jpg,json,js,webmanifest}"
   ],
-  "swDest": "sw.js"
+  globIgnores: ['legacy/**'],
+  swDest: "sw.js",
+  runtimeCaching: [
+    {
+      urlPattern: /.*(?:api\.tiles|api|a\.tiles)\.mapbox\.com/,
+      handler: 'cacheFirst'
+    },
+    {
+      urlPattern: /.*farese\.com\/legacy/,
+      urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+      urlPattern: 'https://cdn.google.com/example-script.min.js',
+      urlPattern: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+      urlPattern: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+      urlPattern: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+      handler: 'staleWhileRevalidate'
+    }
+  ]
 };
