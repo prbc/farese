e4ec3e113fb1e0d13c221debc6d913ddbfd9ce3f
David Birks <david@tellus.space>
2019-09-28 23:59:54 -0400

    Remove sw from build script

--- build/prepare.sh
+++ build/prepare.sh
@@ -4,11 +4,4 @@
 echo "Updating the legacy directory"
 cd legacy/
 python2 generate-text-directory.py
-cd ..
-
-# Update service worker cache hashes
-echo "Updating the service worker cached file hashes"
-workbox generateSW workbox-config.js
-
-echo "Complete"
 
