f47ec799175d66008f0f2564b8d181c956d58d53
David Birks <david@tellus.space>
2019-09-29 00:04:55 -0400

    Revert "Remove sw from build script"
    This reverts commit e4ec3e113fb1e0d13c221debc6d913ddbfd9ce3f.

--- build/prepare.sh
+++ build/prepare.sh
@@ -4,4 +4,11 @@
 echo "Updating the legacy directory"
 cd legacy/
 python2 generate-text-directory.py
+cd ..
+
+# Update service worker cache hashes
+echo "Updating the service worker cached file hashes"
+workbox generateSW workbox-config.js
+
+echo "Complete"
 
