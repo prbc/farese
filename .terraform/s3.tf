26f76480c5a937579abce2f833a165b6067e4ae8
David Birks <david@birks.dev>
2020-01-09 23:20:53 -0500

    Allow public access to s3 bucket while still fronted by cloudflare

--- .terraform/s3.tf
+++ .terraform/s3.tf
@@ -2,7 +2,7 @@
 
 resource "aws_s3_bucket" "farese" {
   bucket = "farese.com"
-  acl    = "private"
+  acl    = "public"
 
   versioning {
     enabled = false
@@ -46,10 +46,10 @@ POLICY
 resource "aws_s3_bucket_public_access_block" "farese" {
   bucket = aws_s3_bucket.farese.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 # dev.farese.com
