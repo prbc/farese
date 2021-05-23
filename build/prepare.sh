ee81d848227a1a783410451b2300c10c80a0a9f4
David Birks <david@birks.dev>
2021-05-22 23:21:41 -0400

    Exit the prepare script with error if any part fails

--- build/prepare.sh
+++ build/prepare.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# Exit immediately on error
+set -o errexit
+
 # Update Legacy site
 echo "Updating the legacy directory"
 cd legacy/
 python2 generate-text-directory.py
-cd ..
+cd -
 
 echo "Complete"
-
