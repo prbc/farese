15a9a72dc88ef6cd4689f0604ab926a7333053ce
David Birks <david@birks.dev>
2023-02-11 23:33:19 -0500

    Fix build issues

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "../assets/react.svg";
 
 function App() {
   const [count, setCount] = useState(0);
